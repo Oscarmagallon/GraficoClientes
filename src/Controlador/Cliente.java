29e0c58ed9a5417fa799c5022bd07b3fe6b73ab7
OSCAR <SI>
2021-04-14 11:52:07 +0200

    actualizacion

---
+++ src/Controlador/Cliente.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author alumnodaw
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String estadoCivil;
+    private String sexo;
+    private int edad;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido1
+     */
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    /**
+     * @param apellido1 the apellido1 to set
+     */
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    /**
+     * @return the apellido2
+     */
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    /**
+     * @param apellido2 the apellido2 to set
+     */
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    /**
+     * @return the estadoCivil
+     */
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    /**
+     * @param estadoCivil the estadoCivil to set
+     */
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
